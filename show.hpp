7e502dc07a93c183d05b7749b67ef75bedd61c4d
soheil <soheilshabani.79.ss@gmail.com>
2022-08-09 11:37:52 +0430

    added support for showing files as polygon to show.hpp header file

--- show.hpp
+++ show.hpp
@@ -1,29 +1,39 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Triangulation_2.h>
 #include <CGAL/draw_triangulation_2.h>
+#include <CGAL/Polygon_2.h>
+#include <CGAL/draw_polygon_2.h>
 #include <fstream>
 #include <vector>
 //#include "./dcel.hpp"
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Triangulation_2<K>                            Triangulation;
-typedef Triangulation::Point                                Point;
+typedef Triangulation::Point                                T_Point;
 
-void show(vector<vertex> ps)
+typedef CGAL::Polygon_2 <K>                                  Polygon_2;
+typedef CGAL::Point_2<K>                                    P_Point;
+
+void show(vector<vertex> ps, std::string type = "triangulation")
 {
-  // create a polygon and put some points in it
-  Triangulation t;
-
-  for (int i=0; i<ps.size(); i++)
+  // create a Triangulation object and put some points in it
+  if (type == "polygon")
   {
-    t.insert(Point(ps[i].x,ps[i].y));
+    Polygon_2 p;
+    for (int i=0; i<ps.size(); i++)
+    {
+      p.push_back(P_Point(ps[i].x,ps[i].y));
+    }
+    CGAL::draw(p);
   }
-  /*
-  p.push_back(Point(0,0));
-  p.push_back(Point(4,0));
-  p.push_back(Point(4,4));
-  p.push_back(Point(2,2));
-  p.push_back(Point(0,4));
-  */
-  CGAL::draw(t);
+  else if (type == "triangulation")
+  {
+    Triangulation t;
+    for (int i=0; i<ps.size(); i++)
+    {
+      t.insert(T_Point(ps[i].x,ps[i].y));
+    }
+    CGAL::draw(t);
+  }
+
 }
